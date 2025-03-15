a8b610f6de568e1e14a37ac3a2072e5673832eb6
Kadin <kanislupus36@protonmail.com>
2025-03-15 14:31:11 -0600

    week 2 assignment

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
 
+routes.use('/', require('./swagger'));
+
 routes.get('/', lesson1Controller.kanisRoute);
 routes.get('/rufus', lesson1Controller.rufusRoute);
 routes.use('/', require('./contacts'));
